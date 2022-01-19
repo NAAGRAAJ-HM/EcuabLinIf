1a008821a25fc75a1422e3e21c6f408109188c02
root <nagaraja.hm.0001@gmail.com>
2022-01-19 11:14:26 +0100

    Update: submodule LinIf

--- LATEST/LinIf.cpp
+++ LATEST/LinIf.cpp
@@ -5,3 +5,42 @@ class_LinIf LinIf;
 FUNC(void, LINIF_CODE) class_LinIf::InitFunction(void){
 }
 
+FUNC(void, LINIF_CODE) class_LinIf::GetVersionInfo(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::Transmit(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::ScheduleRequest(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::GoToSleep(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::Wakeup(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::GetTrcvMode(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::SetTrcvMode(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::GetTrcvWakeupReason(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::SetTrcvWakeupMode(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::CancelTransmit(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::CheckWakeup(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::CbWakeupConfirmation(void){
+}
+
+FUNC(void, LINIF_CODE) class_LinIf::MainFunction(void){
+}
+
--- LATEST/LinIf.h
+++ LATEST/LinIf.h
@@ -5,7 +5,20 @@
 
 class class_LinIf{
    public:
-      FUNC(void, LINIF_CODE) InitFunction(void);
+      FUNC(void, LINIF_CODE) InitFunction         (void);
+      FUNC(void, LINIF_CODE) GetVersionInfo       (void);
+      FUNC(void, LINIF_CODE) Transmit             (void);
+      FUNC(void, LINIF_CODE) ScheduleRequest      (void);
+      FUNC(void, LINIF_CODE) GoToSleep            (void);
+      FUNC(void, LINIF_CODE) Wakeup               (void);
+      FUNC(void, LINIF_CODE) GetTrcvMode          (void);
+      FUNC(void, LINIF_CODE) SetTrcvMode          (void);
+      FUNC(void, LINIF_CODE) GetTrcvWakeupReason  (void);
+      FUNC(void, LINIF_CODE) SetTrcvWakeupMode    (void);
+      FUNC(void, LINIF_CODE) CancelTransmit       (void);
+      FUNC(void, LINIF_CODE) CheckWakeup          (void);
+      FUNC(void, LINIF_CODE) CbWakeupConfirmation (void);
+      FUNC(void, LINIF_CODE) MainFunction         (void);
 };
 
 extern class_LinIf LinIf;
