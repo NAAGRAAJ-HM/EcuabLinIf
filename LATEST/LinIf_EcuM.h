f8c19228b012feeb2ee98648515a1a4b67e7a69e
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:06:39 +0100

    Update: submodule LinIf

--- LATEST/LinIf_EcuM.h
+++ LATEST/LinIf_EcuM.h
@@ -27,8 +27,8 @@ class interface_LinIf_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, LINIF_CODE) InitFunction   (void);
-      FUNC(void, LINIF_CODE) DeInitFunction (void);
+      virtual FUNC(void, LINIF_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, LINIF_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
