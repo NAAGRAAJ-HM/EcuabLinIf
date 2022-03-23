b1edb51c58a5ee99fcfef72b4f9334380af35ba2
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:07:10 +0100

    Update: AUTOSAR basic software ECU abstraction module LinIf

--- LATEST/LinIf.cpp
+++ LATEST/LinIf.cpp
@@ -40,6 +40,16 @@ class module_LinIf:
       FUNC(void, LINIF_CODE) DeInitFunction (void);
       FUNC(void, LINIF_CODE) GetVersionInfo (void);
       FUNC(void, LINIF_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, LINIF_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
