29fdb881c498e93b585d94258e9bffe66461d3ca
root <nagaraja.hm.0001@gmail.com>
2022-03-28 12:46:42 +0200

    Update: AUTOSAR basic software ECU abstraction module LinIf

--- LATEST/LinIf.cpp
+++ LATEST/LinIf.cpp
@@ -90,7 +90,21 @@ FUNC(void, LINIF_CODE) module_LinIf::GetVersionInfo(void){
 FUNC(void, LINIF_CODE) module_LinIf::MainFunction(void){
 }
 
-#include "LinIf_Unused.hpp"
+class class_LinIf_Unused{
+   public:
+      FUNC(void, LINIF_CODE) GetVersionInfo       (void);
+      FUNC(void, LINIF_CODE) Transmit             (void);
+      FUNC(void, LINIF_CODE) ScheduleRequest      (void);
+      FUNC(void, LINIF_CODE) GoToSleep            (void);
+      FUNC(void, LINIF_CODE) Wakeup               (void);
+      FUNC(void, LINIF_CODE) GetTrcvMode          (void);
+      FUNC(void, LINIF_CODE) SetTrcvMode          (void);
+      FUNC(void, LINIF_CODE) GetTrcvWakeupReason  (void);
+      FUNC(void, LINIF_CODE) SetTrcvWakeupMode    (void);
+      FUNC(void, LINIF_CODE) CancelTransmit       (void);
+      FUNC(void, LINIF_CODE) CheckWakeup          (void);
+      FUNC(void, LINIF_CODE) CbWakeupConfirmation (void);
+};
 
 FUNC(void, LINIF_CODE) class_LinIf_Unused::Transmit(void){
 }
