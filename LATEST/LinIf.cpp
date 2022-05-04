f2f6ea5e018ef2046fc5efa48064e67bbfed608a
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:01:07 +0000

    Update: AUTOSAR basic software ECU abstraction module LinIf

--- LATEST/LinIf.cpp
+++ LATEST/LinIf.cpp
@@ -113,7 +113,7 @@ FUNC(void, LINIF_CODE) module_LinIf::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  LINIF_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, LINIF_CODE) module_LinIf::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  LINIF_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, LINIF_CODE) module_LinIf::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  LINIF_E_UNINIT
       );
 #endif
    }
