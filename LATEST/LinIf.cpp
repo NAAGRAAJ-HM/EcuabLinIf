ab6f121c3b775610397aeff835631d2196276f9c
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-11 10:14:04 +0000

    Update: AUTOSAR basic software ECU abstraction module LinIf

--- LATEST/LinIf.cpp
+++ LATEST/LinIf.cpp
@@ -9,7 +9,7 @@
 #include "Module.hpp"
 #include "CfgLinIf.hpp"
 #include "LinIf_core.hpp"
-#include "infLinIf.hpp"
+#include "infLinIf_Exp.hpp"
 
 /******************************************************************************/
 /* #DEFINES                                                                   */
@@ -32,6 +32,7 @@
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
 class module_LinIf:
+   INTERFACES_EXPORTED_LINIF
       public abstract_module
    ,  public class_LinIf_Functionality
 {
