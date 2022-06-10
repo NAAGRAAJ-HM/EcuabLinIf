9420df3387de1db38041e55c7693845efcff8f3d
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-10 13:32:51 +0000

    Update: AUTOSAR basic software ECU abstraction module LinIf

--- LATEST/LinIf.cpp
+++ LATEST/LinIf.cpp
@@ -74,10 +74,6 @@ CONSTP2VAR(infSchMClient, LINIF_VAR, LINIF_CONST) gptrinfSchMClient_LinIf = &Lin
 /******************************************************************************/
 VAR(module_LinIf, LINIF_VAR) LinIf(
    {
-#if(STD_ON == _ReSIM)
-         "LinIf",
-#else
-#endif
          LINIF_AR_RELEASE_VERSION_MAJOR
       ,  LINIF_AR_RELEASE_VERSION_MINOR
       ,  0x00
