d0e608910bc9db27f6eb971d2bddf2a61ba9cd4f
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-04 19:55:07 +0200

    Update: AUTOSAR basic software ECU abstraction module for LIN communication protocol stack - interface layer

---
+++ main_LATEST/EcuabLinIf_core.hpp
@@ -0,0 +1,72 @@
+#pragma once
+/******************************************************************************/
+/* File   : EcuabLinIf_core.hpp                                                    */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_EcuabLinIf.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define LINIF_CORE_FUNCTIONALITIES                                             \
+              FUNC(void, LINIF_CODE) Transmit             (void);              \
+              FUNC(void, LINIF_CODE) ScheduleRequest      (void);              \
+              FUNC(void, LINIF_CODE) GoToSleep            (void);              \
+              FUNC(void, LINIF_CODE) Wakeup               (void);              \
+              FUNC(void, LINIF_CODE) GetTrcvMode          (void);              \
+              FUNC(void, LINIF_CODE) SetTrcvMode          (void);              \
+              FUNC(void, LINIF_CODE) GetTrcvWakeupReason  (void);              \
+              FUNC(void, LINIF_CODE) SetTrcvWakeupMode    (void);              \
+              FUNC(void, LINIF_CODE) McalCancelTransmit       (void);              \
+              FUNC(void, LINIF_CODE) CheckWakeup          (void);              \
+              FUNC(void, LINIF_CODE) CbWakeupConfirmation (void);              \
+
+#define LINIF_CORE_FUNCTIONALITIES_VIRTUAL                                     \
+      virtual FUNC(void, LINIF_CODE) Transmit             (void) = 0;          \
+      virtual FUNC(void, LINIF_CODE) ScheduleRequest      (void) = 0;          \
+      virtual FUNC(void, LINIF_CODE) GoToSleep            (void) = 0;          \
+      virtual FUNC(void, LINIF_CODE) Wakeup               (void) = 0;          \
+      virtual FUNC(void, LINIF_CODE) GetTrcvMode          (void) = 0;          \
+      virtual FUNC(void, LINIF_CODE) SetTrcvMode          (void) = 0;          \
+      virtual FUNC(void, LINIF_CODE) GetTrcvWakeupReason  (void) = 0;          \
+      virtual FUNC(void, LINIF_CODE) SetTrcvWakeupMode    (void) = 0;          \
+      virtual FUNC(void, LINIF_CODE) McalCancelTransmit       (void) = 0;          \
+      virtual FUNC(void, LINIF_CODE) CheckWakeup          (void) = 0;          \
+      virtual FUNC(void, LINIF_CODE) CbWakeupConfirmation (void) = 0;          \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_EcuabLinIf_Functionality{
+   public:
+      LINIF_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
